9af8b7f620a0d22f570c92caef78591c9cac9c6d
tankxie <tankxie@webank.com>
2016-12-10 12:42:33 +0800

    [Task]init commit

---
+++ WBSeekChildren/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WBSeekChildren
+//
+//  Created by tank on 10/12/2016.
+//  Copyright © 2016 webank. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WBSeekChildren/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WBSeekChildren
+//
+//  Created by tank on 10/12/2016.
+//  Copyright © 2016 webank. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
